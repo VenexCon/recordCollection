17b4c0034a20e7ddf7e0451b9386c4a59ed343a6
Connor Davies <venexconsultant@outlook.com>
2022-03-26 20:42:53 +0000

    <update> if statements for two and three rules completed, tested and working

--- rcScript.js
+++ rcScript.js
@@ -31,22 +31,23 @@ const recordCollection = {
 
 
   //Your function must always return the entire record collection object.
-//If prop isn't tracks and value isn't an empty string, update or set that album's prop to value -- Done!
-//If prop is tracks but the album doesn't have a tracks property, create an empty array and add value to it.
+
 //If prop is tracks and value isn't an empty string, add value to the end of the album's existing tracks array.
 //If value is an empty string, delete the given prop property from the album.
 
 
+//If prop isn't tracks and value isn't an empty string, update or set that album's prop to value -- Done!
+          if (prop !== "tracks" && value !== "") {
+            recordCollection[id][prop] = value;
+          } else "false"
+          
+//If prop is tracks but the album doesn't have a tracks property, create an empty array and add value to it.
+
+          if (prop == "tracks" && recordCollection[id].hasOwnProperty("tracks") != true ){
+            recordCollection[id][prop] = [value];
+         } else "null";
 
 
-        if (prop !== tracks && value !== "") {
-            if (recordCollection.id.hasOwnProperty(prop)){
-                recordCollection.id.prop = value;
-            }  else recordCollection.id.prop = value;
-
-
-
-            
-        } else null;
-
-  
+ //If prop is tracks and value isn't an empty string, add value to the end of the album's existing tracks array.
+         
+         if (prop == "tracks" && recordCollection[id].hasOwnProperty(prop) !== ""){}
